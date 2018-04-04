a41244480d255770e3a3af26eeb43ee8ccc18c78
Hussein Hazimeh <hh@ieee.org>
2018-04-04 13:11:33 -0400

    Fix mismatch between AutoLambda0 and LambdaU

--- R/Interface.R
+++ R/Interface.R
@@ -42,7 +42,8 @@ L0Learn.fit <- function(X,y, Loss="SquaredError", Penalty="L0", Algorithm="CD", 
 						GammaMax=10, GammaMin=0.0001, PartialSort = TRUE, MaxIters=200,
 						Tol=1e-6, ActiveSet=TRUE, ActiveSetNum=3, MaxSwaps=100, ScaleDownFactor=0.8, ScreenSize=1000, AutoLambda0 = FALSE, Lambda0Grid = c(0))
 {
-	G <- .Call('_L0Learn_L0LearnFit', PACKAGE = 'L0Learn', X, y, Loss, Penalty, Algorithm, MaxSuppSize, NLambda, NGamma, GammaMax, GammaMin, PartialSort, MaxIters, Tol, ActiveSet, ActiveSetNum, MaxSwaps, ScaleDownFactor, ScreenSize, AutoLambda0, Lambda0Grid)
+	# The C++ function uses LambdaU = 1 for user-specified grid. In R, we use AutoLambda0 = 0 for user-specified grid (thus the negation when passing the paramter to the function below)
+	G <- .Call('_L0Learn_L0LearnFit', PACKAGE = 'L0Learn', X, y, Loss, Penalty, Algorithm, MaxSuppSize, NLambda, NGamma, GammaMax, GammaMin, PartialSort, MaxIters, Tol, ActiveSet, ActiveSetNum, MaxSwaps, ScaleDownFactor, ScreenSize, !AutoLambda0, Lambda0Grid)
 
 	class(G) <- "L0Learn"
 	G$.n <- dim(X)[1]
