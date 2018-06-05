55a001ceadd735faf5fbd2fd0ffa69b8af93103e
Hussein Hazimeh <hh@ieee.org>
2018-06-05 04:53:33 -0400

    Add new implementation for logistic swaps

--- src/CDL012LogisticSwaps.cpp
+++ src/CDL012LogisticSwaps.cpp
@@ -54,124 +54,84 @@ FitResult CDL012LogisticSwaps::Fit()
             // Remove j
             arma::vec ExpyXBnoj = ExpyXB % arma::exp( - B[j] *  Xy->unsafe_col(j));
 
-
-            for(unsigned int i = 0; i < p; ++i)
+            ///
+            arma::rowvec gradient = - arma::sum( Xy->each_col() / (1 + ExpyXBnoj) , 0); // + twolambda2 * Biold // sum column-wise
+            arma::uvec indices = arma::sort_index(arma::abs(gradient),"descend");
+            bool foundbetteri = false;
+            ///
+            for(unsigned int ll = 0; ll < std::min(100, (int) p); ++ll)
             {
+                unsigned int i = indices(ll);
                 if(B[i] == 0)
                 {
                     arma::vec ExpyXBnoji = ExpyXBnoj;
 
                     double Biold = 0;
                     double Binew;
-                    double partial_i = - arma::sum( (Xy->unsafe_col(i)) / (1 + ExpyXBnoji) ); // + twolambda2 * Biold
-
+                    //double partial_i = - arma::sum( (Xy->unsafe_col(i)) / (1 + ExpyXBnoji) ); // + twolambda2 * Biold
+                    double partial_i = gradient[i];
                     bool Converged = false;
-                    if (std::abs(partial_i) >= lambda1 + stl0Lc )
-                    {
-                        double partial2_i = arma::sum( (X->unsafe_col(i) % X->unsafe_col(i) % ExpyXBnoji) / ( (1 + ExpyXBnoji) % (1 + ExpyXBnoji) ) ) + twolambda2;
-
+                    //if (std::abs(partial_i) >= lambda1 + stl0Lc )
+                    //{
                         //std::cout<<"Adding: "<<i<< std::endl;
-
-                        unsigned int l = 0;
                         arma::sp_mat Btemp = B;
                         Btemp[j] = 0;
                         double ObjTemp = Objective(ExpyXBnoji, Btemp);
-                        double NewtonStepSize = 1.0;
                         double Biolddescent = 0;
                         while(!Converged)
                         {
-                            //double x = Biold - partial_i/qp2lamda2;
-
-                            double x;
-                            double TruncatedNewtonStep = NewtonStepSize / partial2_i;
-                            if (TruncatedNewtonStep > 1 / qp2lamda2) // if truncated step is larger use it
-                            {
-                                x = Biold - NewtonStepSize * partial_i / partial2_i;
-                            }
-                            else
-                            {
-                                //std::cout<<"Netwon: "<<TruncatedNewtonStep<<" Normal: "<<1/qp2lamda2<<std::endl;
-                                //std::cout<<"Truncated Newton is smaller!"<<std::endl;
-                                x = Biold - partial_i / qp2lamda2;
-                            }
-
-
+                            double x = Biold - partial_i/qp2lamda2;
                             double z = std::abs(x) - lambda1ol;
-
                             Binew = std::copysign(z, x); // no need to check if >= sqrt(2lambda_0/Lc)
-                            ExpyXBnoji %= arma::exp( (Binew - Biold) *  *y % X->unsafe_col(i));
-                            //std::cout<<"Here222!!"<<std::endl;
 
-
-                            partial_i = - arma::sum( (Xy->unsafe_col(i)) / (1 + ExpyXBnoji) ) + twolambda2 * Binew;
-                            partial2_i = arma::sum( (X->unsafe_col(i) % X->unsafe_col(i) % ExpyXBnoji) / ( (1 + ExpyXBnoji) % (1 + ExpyXBnoji) ) ) + twolambda2;
-
-                            //if (std::abs((Binew - Biold)/Biold) < 0.000001){Converged = true;}
-
-
-                            if (l % 3 == 0)
+                            if (ObjTemp < Fmin)
                             {
-                                Btemp[i] = Binew;
-                                double ObjTempOld = ObjTemp;
-                                ObjTemp = Objective(ExpyXBnoji, Btemp);
-                                //std::cout<<"O: "<<ObjTemp<<" Gamma "<<NewtonStepSize<<std::endl;
-                                if (ObjTemp > ObjTempOld)
-                                {
-                                    NewtonStepSize /= 2.0;
-                                    Binew = Biolddescent;
-                                    ObjTemp = ObjTempOld;
-                                }
-                                else
-                                {
-                                    // If the objective decrease set Biolddescent
-                                    Biolddescent = Binew;
-                                }
-                                if (std::abs(ObjTemp - ObjTempOld) / ObjTempOld < 0.0001) {Converged = true;}
+                                Fmin = ObjTemp;
+                                maxindex = i;
+                                Bmaxindex = Binew;
+                                foundbetteri = true;
+                                break;
                             }
+
+                            ExpyXBnoji %= arma::exp( (Binew - Biold) *  Xy->unsafe_col(i));
+                            partial_i = - arma::sum( (Xy->unsafe_col(i)) / (1 + ExpyXBnoji) ) + twolambda2 * Binew;
+
+                            //if (std::abs((Binew - Biold)/Biold) < 0.0001){Converged = true;}
+
+                            Btemp[i] = Binew;
+                            double ObjTempOld = ObjTemp;
+                            ObjTemp = Objective(ExpyXBnoji, Btemp);
+
+                            if (std::abs(ObjTemp - ObjTempOld) / ObjTempOld < 0.001) {Converged = true;}
 
                             Biold = Binew;
-                            l += 1;
 
                         }
 
                         // Can be made much faster (later)
                         Btemp[i] = Binew;
-                        //auto l2norm = arma::norm(Btemp,2);
-                        //double Fnew = arma::sum(arma::log(1 + 1/ExpyXBnoji)) + ModelParams[0]*Btemp.n_nonzero + ModelParams[1]*arma::norm(Btemp,1) + ModelParams[2]*l2norm*l2norm;
-                        double Fnew = Objective(ExpyXBnoji, Btemp);
-                        //std::cout<<"Fnew: "<<Fnew<<"Index: "<<i<<std::endl;
-                        if (Fnew < Fmin)
-                        {
-                            Fmin = Fnew;
-                            maxindex = i;
-                            Bmaxindex = Binew;
-                        }
-                    }
+
+                    //}
 
 
 
                 }
-
+                if (foundbetteri == true)
+                {
+                      B[j] = 0;
+                      B[maxindex] = Bmaxindex;
+                      P.InitialSol = &B;
+                      P.b0 = b0;
+                      result = CDL012Logistic(*X, *y, P).Fit();
+                      ExpyXB = result.ExpyXB;
+                      B = result.B;
+                      b0 = result.intercept;
+                      objective = result.Objective;
+                      Fmin = objective;
+                      foundbetter = true;
+                      break;
+                }
             }
-
-            if (Fmin < objective)
-            {
-                B[j] = 0;
-                B[maxindex] = Bmaxindex;
-                P.InitialSol = &B;
-                P.b0 = b0;
-
-                result = CDL012Logistic(*X, *y, P).Fit();
-                ExpyXB = result.ExpyXB;
-                B = result.B;
-                b0 = result.intercept;
-                objective = result.Objective;
-                Fmin = objective;
-                foundbetter = true;
-                break;
-
-            }
-
         }
 
         if(!foundbetter) {result.Model = this; return result;}
