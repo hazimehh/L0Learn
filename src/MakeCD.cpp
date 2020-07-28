7997a9e07ccb8eb0f34353c0ae83848ef5763494
Tim Nonet <tim.nonet@gmail.com>
2020-07-28 14:47:28 -0400

    Move MakeCD.cpp include to inside MakeCD.h

--- src/MakeCD.cpp
+++ src/MakeCD.cpp
@@ -1,11 +1,4 @@
 #include "MakeCD.h"
-#include "CDL0.h"
-#include "CDL012.h"
-#include "CDL012Swaps.h"
-#include "CDL012Logistic.h"
-#include "CDL012SquaredHinge.h"
-#include "CDL012LogisticSwaps.h"
-#include "CDL012SquaredHingeSwaps.h"
 
 
 CD * make_CD(const arma::mat& Xi, const arma::vec& yi, const Params& P)
--- src/include/MakeCD.h
+++ src/include/MakeCD.h
@@ -2,8 +2,15 @@
 #define MAKECD_H
 #include "Params.h"
 #include "CD.h"
+#include "CDL0.h"
+#include "CDL012.h"
+#include "CDL012Swaps.h"
+#include "CDL012Logistic.h"
+#include "CDL012SquaredHinge.h"
+#include "CDL012LogisticSwaps.h"
+#include "CDL012SquaredHingeSwaps.h"
 //class CD;
 
 CD * make_CD(const arma::mat& Xi, const arma::vec& yi, const Params& P);
 
-#endif
+#endif 
