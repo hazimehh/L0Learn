fecd8c903b1ba22225901a74c4af4371c53395b4
Tim Nonet <tim.nonet@gmail.com>
2020-07-28 10:41:12 -0400

    Add header guard to Normalize.h

--- src/include/Normalize.h
+++ src/include/Normalize.h
@@ -1,3 +1,5 @@
+#ifndef NORMALIZE_H
+#define NORMALIZE_H
 #include <tuple>
 //#include <armadillo>
 #include "RcppArmadillo.h"
@@ -5,3 +7,5 @@
 std::tuple<arma::vec, arma::vec, double>  Normalize(const arma::mat& X, const arma::vec& y, arma::mat & X_normalized, arma::vec & y_normalized, bool Normalizey, bool intercept);
 
 std::tuple<arma::sp_mat, double> DeNormalize(arma::sp_mat & B_scaled, arma::vec & BetaMultiplier, arma::vec & meanX, double meany);
+
+#endif // NORMALIZE_H
