5c88e5023ffcf8ec3d88075788082597b67631cf
Hussein Hazimeh <hazimeh@mit.edu>
2020-12-11 21:57:15 -0500

    Update CDL012LogisticSwaps.cpp

--- src/CDL012LogisticSwaps.cpp
+++ src/CDL012LogisticSwaps.cpp
@@ -50,7 +50,7 @@ FitResult CDL012LogisticSwaps::Fit()
         //std::shuffle(std::begin(Order), std::end(Order), engine);
         foundbetter = false;
 
-	arma::uvec screened_order = arma::conv_to< arma::uvec >::from(P.Uorder);
+	arma::uvec screened_order = arma::conv_to< arma::uvec >::from(std::vector<unsigned int>(P.Uorder.begin(), P.Uorder.begin()+100));
 
         for (auto& j : NnzIndices)
         {
