cbfcb0224fce12ad7668dfa1feb35e37592778e1
Tim Nonet <tim.nonet@gmail.com>
2021-08-21 16:53:41 -0500

    refactored for shared src format

---
+++ src/include/CDSwaps.hpp
@@ -0,0 +1,5 @@
+#ifndef CDSWAPS_H
+#define CDSWAPS_H
+#include "CD.hpp"
+
+#endif
--- src/include/Test_Interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef R_TEST_INTERFACE_H
-#define R_TEST_INTERFACE_H
-
-#include <tuple>
-#include "utils.h"
-#include "BetaVector.h"
-
-#endif //R_TEST_INTERFACE_H 
