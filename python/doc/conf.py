af09de6225ccfbe456f9387408b04f5ec5a97749
Tim Nonet <tim.nonet@gmail.com>
2021-12-03 17:28:46 -0600

    Update docs format

--- python/doc/conf.py
+++ python/doc/conf.py
@@ -13,8 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-print(sys.executable)
-
 
 # -- Project information -----------------------------------------------------
 
@@ -34,8 +32,6 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               ]
 
-html_theme = "classic"
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -50,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
